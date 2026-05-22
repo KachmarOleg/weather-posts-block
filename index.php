4df997cad0d5a003ebc665f36b46f1d24020b87c
oleh.kachmar <oleh.kachmar@castfromclay.co.uk>
2026-05-22 20:20:51 +0200

    Add customizable post selection and styling for them

--- index.php
+++ index.php
@@ -11,13 +11,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Register Gutenberg block
-function weather_posts_block_register() {
+function weather_posts_block_register(): void
+{
 
     wp_register_script(
         'weather-posts-block-editor',
         plugins_url( 'block.js', __FILE__ ),
         array( 'wp-blocks', 'wp-element', 'wp-block-editor' ),
         filemtime( plugin_dir_path( __FILE__ ) . 'block.js' )
+    );
+
+    wp_enqueue_style(
+        'weather-posts-block-fonts',
+        'https://fonts.googleapis.com/css2?family=Archivo:wght@100;200;300;400;500;600;700;800;900&display=swap',
+        [],
+        null
     );
 
     wp_register_style(
@@ -40,14 +48,35 @@ function weather_posts_block_register() {
 add_action( 'init', 'weather_posts_block_register' );
 
 // Render block
-function weather_posts_block_render( $attributes, $content ) {
+function weather_posts_block_render( $attributes ): string
+{
+    $plugin_dir_path = plugin_dir_path( __FILE__ );
+
+    $big_id     = !empty($attributes['bigPostId']) ? (int)$attributes['bigPostId'] : 0;
+    $small1_id  = !empty($attributes['smallPost1Id']) ? (int)$attributes['smallPost1Id'] : 0;
+    $small2_id  = !empty($attributes['smallPost2Id']) ? (int)$attributes['smallPost2Id'] : 0;
 
     ob_start();
     ?>
 
     <div class="weather-posts-block">
-        <div class="container">
-            <h2>Hello world</h2>
+        <div class="container-grid">
+            <?php $args = [ 'post_id' => $big_id ];
+            if ( file_exists( $plugin_dir_path ) ) {
+                include $plugin_dir_path . 'templates/post-item.php';
+            } ?>
+
+            <div class="small-posts">
+                <?php $args = [ 'post_id' => $small1_id ];
+                if ( file_exists( $plugin_dir_path ) ) {
+                    include $plugin_dir_path . 'templates/post-item.php';
+                } ?>
+
+                <?php $args = [ 'post_id' => $small2_id ];
+                if ( file_exists( $plugin_dir_path ) ) {
+                    include $plugin_dir_path . 'templates/post-item.php';
+                } ?>
+            </div>
         </div>
     </div>
 
---
+++ templates/post-item.php
@@ -0,0 +1,40 @@
+<?php
+$post_id = ! empty( $args['post_id'] ) ? $args['post_id'] : get_the_ID();
+$big     = get_post( $post_id );
+
+$permalink  = get_permalink( $big );
+$title      = esc_html( $big->post_title );
+$excerpt    = wp_trim_words( $big->post_excerpt ?: $big->post_content, 30 );
+
+$categories   = get_the_category( $big->ID );
+$cat_names    = ! empty( $categories ) ? wp_list_pluck( $categories, 'name' ) : [];
+?>
+
+
+<article class="post-card post-card--featured">
+    <?php if ( has_post_thumbnail( $big ) ) : ?>
+        <div class="post-card__thumbnail">
+            <a href="<?php echo esc_url( $permalink ); ?>" tabindex="-1" aria-hidden="true">
+                <?php echo get_the_post_thumbnail( $big, 'large', [ 'class' => 'post-card__image' ] ); ?>
+            </a>
+        </div>
+    <?php endif; ?>
+
+    <div class="post-card__body">
+        <?php if ( ! empty( $cat_names ) ) : ?>
+            <div class="post-card__categories">
+                <?php foreach ( $cat_names as $cat_name ) : ?>
+                    <span class="post-card__category"><?php echo esc_html( $cat_name ); ?></span>
+                <?php endforeach; ?>
+            </div>
+        <?php endif; ?>
+
+        <h2 class="post-card__title">
+            <a class="post-card__link" href="<?php echo esc_url( $permalink ); ?>">
+                <?php echo $title; ?>
+            </a>
+        </h2>
+
+        <p class="post-card__excerpt"><?php echo $excerpt; ?></p>
+    </div>
+</article>
